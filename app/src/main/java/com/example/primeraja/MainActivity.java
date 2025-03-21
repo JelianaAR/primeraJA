a25f45991c0fd809ce7532e4103cc2779de8c836
jeliana <jeliar0731@gmail.com>
2025-03-20 19:37:08 -0500

    update 2

--- app/src/main/java/com/example/primeraja/MainActivity.java
+++ app/src/main/java/com/example/primeraja/MainActivity.java
@@ -1,58 +1,70 @@
 package com.example.primeraja;
 
+import android.graphics.Color;
+import java.util.Random;
 import android.widget.SeekBar;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-    private RadioGroup radioGroup;
     private Button button;
     private SeekBar seekBar;
+
+    private Button button2;
+    private final Random RANDOM = new Random();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editText = findViewById(R.id.editTextText);
-        radioGroup = findViewById(R.id.radioGroup3);
         button = findViewById(R.id.button);
         seekBar = findViewById(R.id.seekBar3);
+        button2 = findViewById(R.id.button2);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float textSize = 12 + progress;
+                float textSize = 10 + progress;
                 editText.setTextSize(textSize);
                 editText.setHint("Tamaño: " + textSize + "sp");
             }
-
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
-
+            public void onStartTrackingTouch(SeekBar seekBar) {}
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
-
+        int[] fuentes = {
+                R.font.dmserif,
+                R.font.doto,
+                R.font.caveat
+        };
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-                if (selectedId != -1) {
-                    RadioButton selectedRadioButton = findViewById(selectedId);
-                    String fontName = selectedRadioButton.getTag().toString();
-                    int fontId = getResources().getIdentifier(fontName, "font", getPackageName());
-                    Typeface typeface = ResourcesCompat.getFont(MainActivity.this, fontId);
-                    editText.setTypeface(typeface);
-                }
+                int fuenteAleatoria = fuentes[(int) (Math.random() * fuentes.length)];
+                Typeface typeface = ResourcesCompat.getFont(MainActivity.this, fuenteAleatoria);
+                editText.setTypeface(typeface);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = RANDOM.nextInt(256);
+                int green = RANDOM.nextInt(256);
+                int blue = RANDOM.nextInt(256);
+                int color = Color.rgb(red, green, blue);
+                editText.setTextColor(color);
             }
         });
     }
 }
+
